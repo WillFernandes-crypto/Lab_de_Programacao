93d5fe26ba4317812e7241c699554a93381202b1
Wilson Fernandes <wilsonfernandes390@gmail.com>
2024-11-19 02:10:04 -0300

    Atualizações nos códigos e arquivos

--- core/game.py
+++ core/game.py
@@ -1,60 +1,34 @@
-'''# core/game.py
+# core/game.py
 import pygame
-from entities.player import Player
-from entities.player import Sword
-from levels.initial_level import InitialLevel
+from utils.settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from levels.level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
+import sys  # Para permitir encerramento correto do jogo
 
 class Game:
-    def __init__(self, screen, screen_width, screen_height, gravity):
-        self.screen = screen
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.level = InitialLevel(screen_width, screen_height)
-        self.player = Player(200, 200, 2, 5, gravity)
+    def __init__(self):
+        # Inicializa o Pygame e configura o jogo
+        self.display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+        pygame.display.set_caption("The Emptiness Machine")
         self.clock = pygame.time.Clock()
-        self.FPS = 60
-        self.is_fullscreen = False
-
-    def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_F11:
-                self.toggle_fullscreen()
-
-        if event.type == pygame.VIDEORESIZE:
-            self.screen_width = event.w
-            self.screen_height = event.h
-            self.level.update_size(self.screen_width, self.screen_height)
-
-    def toggle_fullscreen(self):
-        if self.is_fullscreen:
-            self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.RESIZABLE)
-            self.is_fullscreen = False
-        else:
-            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            self.is_fullscreen = True
+        
+        # Carrega os mapas
+        self.tmx_maps = {
+            0: load_pygame(join('assets', 'data', 'levels', 'omni.tmx'))
+        }
+        # Configura o nível inicial
+        self.current_stage = Level(self.tmx_maps[0])
 
     def run(self):
-        self.level.update_scroll()
-        self.level.draw(self.screen)
+        """Loop principal do jogo."""
+        while True:
+            delta_time = self.clock.tick(60) / 1000  # Controla o FPS
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-        self.player.update()
-        self.player.draw(self.screen)
-        self.player.update_animation()
-
-    def update_player(self, moving_left, moving_right, jump, attack, sword_group):
-        if self.player.alive:
-            # Ataque com espada
-            if attack:
-                sword = Sword(self.player.rect.centerx + (0.6 * self.player.rect.size[0] * self.player.direction), 
-                              self.player.rect.centery, self.player.direction)
-                sword_group.add(sword)
-                self.player.update_action(3)  # 3: ataque
-            elif self.player.in_air:
-                self.player.update_action(2)  # 2: jump
-            elif moving_left or moving_right:
-                self.player.update_action(1)  # 1: walk
-            else:
-                self.player.update_action(0)  # 0: idle
-            
-            self.player.move(moving_left, moving_right, jump)
-'''
+            # Atualiza e renderiza o nível atual
+            self.current_stage.run(delta_time)
+            pygame.display.update()
--- main.py
+++ main.py
@@ -1,34 +1,9 @@
 # main.py
 import pygame
-from core.game import *
-from core.input import get_user_input
-from utils.settings import *
-from levels.level import *
-from pytmx.util_pygame import load_pygame
-from os.path import join
-
-class Main:
-    def __init__(self):
-        # Inicializa o Pygame
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
-        pygame.display.set_caption("The Emptiness Machine")
-        self.clock = pygame.time.Clock()
-        self.tmx_maps = {0: load_pygame(join('assets', 'data', 'levels', 'omni.tmx'))}
-        self.current_stage = Level(self.tmx_maps[0])
-
-    def run(self):
-        # Inicia o loop do jogo
-        while True:
-            delta_time = self.clock.tick(60) / 1000
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            self.current_stage.run(delta_time)
-            pygame.display.update()
+from core.game import Game
 
 if __name__ == '__main__':
-    game = Main()
+    pygame.init()
+    game = Game()
     game.run()
+    pygame.quit()
