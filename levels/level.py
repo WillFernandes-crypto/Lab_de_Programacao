422772f2492dabd3ad10eab7181d92884b6244a7
Wilson Fernandes <wilsonfernandes390@gmail.com>
2024-11-26 01:39:12 -0300

    Adição de elementos gráficos, background e limites de tela

--- levels/level.py
+++ levels/level.py
@@ -9,9 +9,26 @@ class Level:
     def __init__(self, tmx_map, level_frames, data):
         self.display_surface = pygame.display.get_surface()
         self.data = data
+
+        # Level data
+        self.level_width = tmx_map.width * TILE_SIZE
+        self.level_bottom = tmx_map.height * TILE_SIZE
+        tmx_level_properties = tmx_map.get_layer_by_name('Data')[0].properties
+        self.level_unlock = tmx_level_properties['level_unlock']
+        if tmx_level_properties['bg']:
+            bg_tile = level_frames['bg_tiles'][tmx_level_properties['bg']]
+        else:
+            bg_tile = None
+
         
         # Grupos
-        self.all_sprites = AllSprites()
+        self.all_sprites = AllSprites(
+			width = tmx_map.width, 
+			height = tmx_map.height,
+			bg_tile = bg_tile, 
+			top_limit = tmx_level_properties['top_limit'], 
+			clouds = {'large': level_frames['cloud_large'], 'small': level_frames['cloud_small']},
+			horizon_line = tmx_level_properties['horizon_line'])
         self.collision_sprites = pygame.sprite.Group()
         self.semi_collision_sprites = pygame.sprite.Group()
         self.damage_sprites = pygame.sprite.Group()
@@ -71,7 +88,7 @@ class Level:
                     animation_speed = ANIMATION_SPEED if not 'palm' in obj.name else ANIMATION_SPEED + uniform(-1,1)
                     AnimatedSprite((obj.x, obj.y), frames, groups, z, animation_speed)
             if obj.name == 'flag':
-                self.level_finish_rect = pygame.FRect((obj.x, obj.y), (obj.width, obj.height))
+                self.level_finish_rect = pygame.FRect((obj.x, obj.y), (obj.width, obj.height))    
 
         # Objetos que se movem
         for obj in tmx_map.get_layer_by_name('Moving Objects'):
@@ -143,7 +160,20 @@ class Level:
                  (obj.x + TILE_SIZE / 2, obj.y + TILE_SIZE / 2), 
                  level_frames['items'][obj.name], (self.all_sprites, self.item_sprites), 
                  self.data)
-            
+
+        # Água
+        for obj in tmx_map.get_layer_by_name('Water'):
+            rows = int(obj.height / TILE_SIZE) 
+            cols = int(obj.width / TILE_SIZE) 
+            for row in range(rows):
+                for col in range(cols):
+                    x = obj.x + col * TILE_SIZE
+                    y = obj.y + row * TILE_SIZE
+                    if row == 0:
+                        AnimatedSprite((x,y), level_frames['water_top'], self.all_sprites, Z_LAYERS['water'])
+                    else:
+                        Sprite((x,y), level_frames['water_body'], self.all_sprites, Z_LAYERS['water'])
+
     def create_pearl(self, pos, direction):
         Pearl(pos, (self.all_sprites, self.damage_sprites, self.pearl_sprites), self.pearl_surf, direction, 150)
 
@@ -180,6 +210,21 @@ class Level:
             if target.rect.colliderect(self.player.rect) and self.player.attacking and facing_target:
                 target.reverse()
 
+    def check_constraint(self):
+        # esqueda direita
+        if self.player.hitbox_rect.left <= 0:
+            self.player.hitbox_rect.left = 0
+        if self.player.hitbox_rect.right >= self.level_width:
+            self.player.hitbox_rect.right = self.level_width
+
+        # borda abaixo 
+        if self.player.hitbox_rect.bottom > self.level_bottom:
+            self.switch_stage('overworld', -1)
+
+        # se der certo... 
+        if self.player.hitbox_rect.colliderect(self.level_finish_rect):
+            self.switch_stage('overworld', self.level_unlock)
+
     def run(self, delta_time):
         self.display_surface.fill('gray')
 
@@ -189,4 +234,4 @@ class Level:
         self.item_collision()
         self.attack_collision()
 
-        self.all_sprites.draw(self.player.hitbox_rect.center)
+        self.all_sprites.draw(self.player.hitbox_rect.center, delta_time)
